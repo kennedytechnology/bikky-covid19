a1a0d61c43666738ee4a46abfe29516aabacc0ab
Orce <orce@kennedytech.mk>
2020-03-16 00:36:52 +0100

    Added seeds for active admin user with ENV VAR

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 require 'csv'
 
 csv_text = File.read(Rails.root.join('lib', 'seeds', 'restaurants.csv'))
-csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
+csv = CSV.parse(csv_text, headers: true, encoding: 'ISO-8859-1')
 csv.each do |row|
   t = Restaurant.new
   t.name = row['name']
@@ -12,5 +12,9 @@ csv.each do |row|
   t.save
 end
 
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+if ENV["DEFAULT_ADMIN_PASSWORD"]
+  AdminUser.create!(email: "admin@project-facemask.com", password: ENV["DEFAULT_ADMIN_PASSWORD"], password_confirmation: ENV["DEFAULT_ADMIN_PASSWORD"])
+else
+  AdminUser.create!(email: "admin@example.com", password: "password", password_confirmation: "password")
+end
 puts "Done"
