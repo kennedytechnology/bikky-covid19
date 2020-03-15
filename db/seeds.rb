37c512e60fbc1b26631a7504fad70a47ca535e07
Violeta P <32481414+violeta-p@users.noreply.github.com>
2020-03-15 22:35:51 +0100

    Add real data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,15 @@
-30.times do |i|
-  Restaurant.create!(
-    name: Faker::Restaurant.name,
-    address: Faker::Address.full_address,
-    description: Faker::Lorem.paragraph(sentence_count: Faker::Number.between(from: 1, to: 6)),
-    latitude: Faker::Address.latitude,
-    longitude: Faker::Address.longitude
-  )
+require 'csv'
+
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'restaurants.csv'))
+csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
+csv.each do |row|
+  t = Restaurant.new
+  t.name = row['name']
+  t.location = row['location']
+  t.address = row['address']
+  t.url = row['url']
+  t.description = 
+  t.save
 end
+
+puts "Done"
