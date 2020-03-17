a987e769529e36d85b3c5f6628850f5619496a98
Violeta P <32481414+violeta-p@users.noreply.github.com>
2020-03-17 19:46:37 +0100

    Improve handling search form for guides

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,16 @@
 class PagesController < ApplicationController
+  before_action :find_restaurants, only: [:guides]
+  
   def guides
+  end
+
+  private
+
+  def find_restaurants
     if params[:q].blank?
       @restaurants = Restaurant.all.group_by { |r| r.name }
     else
-      @restaurants = Restaurant.near(params[:q], 0.5, order: :distance)
+      @restaurants = Restaurant.near(params[:q], 0.5, order: :distance).group_by { |r| r.name }
     end
   end
 end
