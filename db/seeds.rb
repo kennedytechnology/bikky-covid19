611f0bf273284b01f50619c439e70ddc635fb54e
Violeta P <32481414+violeta-p@users.noreply.github.com>
2020-03-17 22:28:56 +0100

    Import data for restaurants

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,16 +6,11 @@ CSV.parse(csv_text, headers: true).each do |row|
   Partner.create!(row.to_h)
 end
 
-# csv_text = File.read(Rails.root.join('lib', 'seeds', 'restaurants.csv'))
-# csv = CSV.parse(csv_text, headers: true)
-# csv.each do |row|
-#   t = Restaurant.new
-#   t.name = row['name']
-#   t.location = row['location']
-#   t.address = row['address']
-#   t.url = row['url']
-#   t.description = Faker::Lorem.paragraph(sentence_count: Faker::Number.between(from: 1, to: 6))
-#   t.save
-# end
+csv_text = File.read(Rails.root.join('lib', 'seeds', 'restaurants.csv'))
+csv = CSV.parse(csv_text, headers: true)
+csv.each do |row|
+  row['partner'] = Partner.find_by_brand(row.delete('brand'))
+  Restaurant.create!(row.to_h)
+end
 
 puts "Done"
