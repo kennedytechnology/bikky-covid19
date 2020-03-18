9ee3619b2c623b62ddad17a1ba97fd8b67b2b565
Violeta P <32481414+violeta-p@users.noreply.github.com>
2020-03-18 12:04:45 +0100

    Display guide for restaurants

--- app/controllers/guides_controller.rb
+++ app/controllers/guides_controller.rb
@@ -1,16 +1,5 @@
 class GuidesController < ApplicationController
-  before_action :find_restaurants, only: [:index]
-  
   def index
-  end
-
-  private
-
-  def find_restaurants
-    if params[:q].blank?
-      @restaurants = Restaurant.all.group_by { |r| r.brand }
-    else
-      @restaurants = Restaurant.near(params[:q], 0.5, order: :distance).group_by { |r| r.brand }
-    end
+    @restaurants = Restaurant.joins(:guides).group_by { |r| r.guides.first.name }
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,23 +12,4 @@ module ApplicationHelper
       return description
     end
   end
-
-  def find_motto(restaurant_name)
-    case restaurant_name
-    when "Dos Toros"
-      return "Because you want an office lunch but are on your couch in your underwear"
-    when "Stone Bridge"
-      return "Because you’re streaming Twitch / eSports more than ever before"
-    when "PB&K" || "Juice Press"
-      return "Because you just gotta get back on your Peloton"
-    when "Bareburger"
-      return "Because food comas are a form of happiness"
-    when "Happiness Foods"
-      return "Because this quarantine ain’t getting me down"
-    when "test"
-      return ""
-    else
-      return restaurant_name
-    end
-  end
 end
