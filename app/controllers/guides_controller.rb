b4299c3b30e34a1ac2ed33a75619660ed58240c8
Violeta P <32481414+violeta-p@users.noreply.github.com>
2020-03-18 12:43:16 +0100

    Improve query for guides

--- app/controllers/guides_controller.rb
+++ app/controllers/guides_controller.rb
@@ -1,5 +1,5 @@
 class GuidesController < ApplicationController
   def index
-    @restaurants = Restaurant.joins(:guides).group_by { |r| r.guides.first.name }
+    @guides = Guide.includes(:restaurants).all
   end
 end
