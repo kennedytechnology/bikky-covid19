149e55a8172118823889f4017b9cd3d965510e08
Violeta P <32481414+violeta-p@users.noreply.github.com>
2020-03-18 09:03:15 +0100

    Shuffle restaurants

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,7 +1,7 @@
 class RestaurantsController < ApplicationController
   def index
     if params[:q].blank?
-      @restaurants = Restaurant.all
+      @restaurants = Restaurant.all.shuffle
     else
       @restaurants = Restaurant.near(params[:q], 0.5, order: :distance)
     end
