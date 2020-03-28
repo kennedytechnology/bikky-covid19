9fc985421c529672444bbc998470acb2e448f1f4
Orce <orce@kennedytech.mk>
2020-03-28 12:48:50 +0100

    Compacting the filters

--- app/admin/partners.rb
+++ app/admin/partners.rb
@@ -14,7 +14,7 @@ ActiveAdmin.register Partner do
     column :meal_size_1
     column :meal_size_2
     column "Price", :price do |partner|
-      "$" * partner.price
+      "$" * partner.price.to_i
     end
 
     actions
@@ -22,10 +22,10 @@ ActiveAdmin.register Partner do
 
   # Filters
   filter :brand, as: :select, collection: proc { Partner.all.collect{|partner| partner.brand}.uniq.sort }
-  filter :mood, as: :select, collection: proc { Partner.all.collect{|partner| partner.mood}.uniq.sort }
-  filter :daypart_1, as: :select, collection: proc { Partner.all.collect{|partner| partner.daypart_1}.uniq }
+  filter :mood, as: :select, collection: proc { Partner.all.collect{|partner| partner.mood}.uniq.compact.sort }
+  filter :daypart_1, as: :select, collection: proc { Partner.all.collect{|partner| partner.daypart_1}.uniq.compact }
   filter :daypart_2, as: :select, collection: proc { Partner.all.collect{|partner| partner.daypart_2}.uniq.compact }
-  filter :meal_size_1, as: :select, collection: proc { Partner.all.collect{|partner| partner.meal_size_1}.uniq }
+  filter :meal_size_1, as: :select, collection: proc { Partner.all.collect{|partner| partner.meal_size_1}.uniq.compact }
   filter :meal_size_2, as: :select, collection: proc { Partner.all.collect{|partner| partner.meal_size_2}.uniq.compact }
   filter :price, as: :select, collection: { '$' => 1, '$$' => 2, '$$$' => 3, '$$$$' => 4, '$$$$$' => 5 }
 
