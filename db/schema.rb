493115fe0d7b14f3b8321c4f9be7d82b45353485
Orce <orce@kennedytech.mk>
2020-03-31 09:49:45 +0200

    Generated AA picture resource

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_31_074418) do
+ActiveRecord::Schema.define(version: 2020_03_31_074506) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -92,6 +92,15 @@ ActiveRecord::Schema.define(version: 2020_03_31_074418) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "partner"
+  end
+
+  create_table "pictures", force: :cascade do |t|
+    t.string "category"
+    t.string "pictureable_type", null: false
+    t.bigint "pictureable_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["pictureable_type", "pictureable_id"], name: "index_pictures_on_pictureable_type_and_pictureable_id"
   end
 
   create_table "restaurants", force: :cascade do |t|
