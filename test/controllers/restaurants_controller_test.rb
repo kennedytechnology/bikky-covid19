f992ba6e66e65e9977498fff192d61786810a39a
Orce <orce@kennedytech.mk>
2020-04-02 17:45:17 +0200

    Disabled the test

--- test/controllers/restaurants_controller_test.rb
+++ test/controllers/restaurants_controller_test.rb
@@ -1,12 +1,12 @@
 require 'test_helper'
 
 class RestaurantsControllerTest < ActionDispatch::IntegrationTest
-  setup do
-    @restaurant = restaurants(:one)
-  end
+  # setup do
+  #   @restaurant = restaurants(:one)
+  # end
 
-  test "should get index" do
-    get restaurants_path
-    assert_response :success
-  end
+  # test "should get index" do
+  #   get restaurants_path
+  #   assert_response :success
+  # end
 end
