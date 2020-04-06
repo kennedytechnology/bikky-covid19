c5a9f226976cf3890808db96fb616860e78896f0
Orce <orce@kennedytech.mk>
2020-04-06 15:54:27 +0200

    Set partner with pictures

--- app/models/partner.rb
+++ app/models/partner.rb
@@ -1,6 +1,9 @@
 class Partner < ApplicationRecord
   has_many :restaurants
   has_and_belongs_to_many :guides
+  has_one :picture, dependent: :destroy
+
+  accepts_nested_attributes_for :picture, reject_if: lambda { |a| a[:image].blank? }, allow_destroy: true
 
   def name; brand; end;
 end
