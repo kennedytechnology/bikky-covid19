8cd94d005d76de8afe107d116820056155655fd1
Orce <orce@kennedytech.mk>
2020-04-07 13:09:14 +0200

    Ability to mass upload restaurants csv through Active Admin index

--- app/admin/restaurants.rb
+++ app/admin/restaurants.rb
@@ -3,6 +3,19 @@ ActiveAdmin.register Restaurant do
 
   permit_params :name, :address, :longitude, :latitude, :partner_id, :url
 
+  action_item :only => :index do
+    link_to 'Upload CSV', :action => 'upload_csv'
+  end
+
+  collection_action :upload_csv do
+    render "admin/csv/upload_csv"
+  end
+
+  collection_action :import_csv, :method => :post do
+    CsvDb.convert_save("restaurant", params[:dump][:file])
+    redirect_to :action => :index, :notice => "CSV imported successfully!"
+  end
+
   # Index
   index download_links: [:csv] do
     selectable_column
