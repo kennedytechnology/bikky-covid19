7d697509ae54e1bd6a7d9190ec88933221aec005
Orce <orce@kennedytech.mk>
2020-04-13 22:31:31 +0200

    Migration to weekly hours

---
+++ db/migrate/20200413150708_add_weekly_hours_to_restaurant.rb
@@ -0,0 +1,8 @@
+class AddWeeklyHoursToRestaurant < ActiveRecord::Migration[6.0]
+  def change
+    add_column :restaurants, :weekday_open, :string
+    add_column :restaurants, :weekday_close, :string
+    add_column :restaurants, :weekend_open, :string
+    add_column :restaurants, :weekend_close, :string
+  end
+end
