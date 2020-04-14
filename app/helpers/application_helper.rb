59f104514164a7f2d04e3244bbe8e6fa3621e9a8
Violeta P <32481414+violeta-p@users.noreply.github.com>
2020-04-14 10:59:36 +0200

    Add helper for link_to with if/else condition

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,15 @@ module ApplicationHelper
       image_url('restaurants_photos/default.jpeg')
     end
   end
+
+  def link_to_cond(condition, name, options = {}, html_options = {}, &block)
+    if condition
+      name = name.call if name.is_a?(Proc)
+      link_to(name, options, html_options, &block)
+    elsif block_given?
+      block.arity <= 1 ? capture(name, &block) : capture(name, options, html_options, &block)
+    else
+      ERB::Util.html_escape(name)
+    end
+  end
 end
