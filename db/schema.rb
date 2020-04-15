c8921c61b9f96b9732c8dda4bfedcafb0712d553
Orce <orce@kennedytech.mk>
2020-04-15 14:42:13 +0200

    Removed status field from partner

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_14_100132) do
+ActiveRecord::Schema.define(version: 2020_04_15_124050) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -81,7 +81,6 @@ ActiveRecord::Schema.define(version: 2020_04_14_100132) do
 
   create_table "partners", force: :cascade do |t|
     t.string "brand"
-    t.string "status"
     t.string "mood"
     t.string "daypart_1"
     t.string "daypart_2"
