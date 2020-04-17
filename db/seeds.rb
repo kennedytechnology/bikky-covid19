b2b405e671d93f25703f0ddd2d6307fde0d0feb4
Orce <orce@kennedytech.mk>
2020-04-17 16:49:43 +0200

    Removed blank option

--- db/seeds.rb
+++ db/seeds.rb
@@ -72,19 +72,19 @@ Guide.find_by_name("Because you really need a cuddle").partners << partners
 mood = ["Chillin with homies", "Home comfort", "It's a family affair", "Keep grindin", "Oh you fancy huh", "TREAT YO SELF", "Too cool for school", "Your body is a temple"]
 mood.each { |m| Mood.create!(name: m) }
 
-daypart1 = ["", "Afternoon Delight", "Breakfast", "Dessert", "Dinner", "Lunch"]
+daypart1 = ["Afternoon Delight", "Breakfast", "Dessert", "Dinner", "Lunch"]
 daypart1.each { |dp1| Daypart1.create!(name: dp1) }
 
-daypart2 = ["", "Afternoon Delight", "Breakfast", "Dessert", "Dinner", "Lunch"]
+daypart2 = ["Afternoon Delight", "Breakfast", "Dessert", "Dinner", "Lunch"]
 daypart2.each { |dp2| Daypart2.create!(name: dp2) }
 
-mealsize1 = ["", "Full House", "Social Distance", "Trapped with roommate"]
+mealsize1 = ["Full House", "Social Distance", "Trapped with roommate"]
 mealsize1.each { |ms1| MealSize1.create!(name: ms1) }
 
-mealsize2 = ["", "Full House", "Social Distance", "Trapped with roommate"]
+mealsize2 = ["Full House", "Social Distance", "Trapped with roommate"]
 mealsize2.each { |ms2| MealSize2.create!(name: ms2) }
 
-deal = ["", "none"]
+deal = ["none"]
 deal.each { |d| Deal.create!(name: d) }
 
 puts "Done"
